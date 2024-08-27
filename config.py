2dc6ae65d5a35026fed37b71e0d8ba50953a423a
Ryczek <miryczkowski@gmail.com>
2024-08-27 13:55:14 +0200

    implement dataset generator

--- config.py
+++ config.py
@@ -2,10 +2,15 @@ import os
 class Config:
     hugging_face_token = "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"
     dataset_root = '16000_pcm_speeches'
-    sampling_rate=16000
+
     audio_subfolder = "audio"
     noise_subfolder = "noise"
-    
+
     dataset_audio_path = os.path.join(dataset_root, audio_subfolder)
     dataset_noise_path = os.path.join(dataset_root, noise_subfolder)
-    
+    valid_split = 0.1
+    shuffle_seed = 43
+    samplig_rate = 16000
+    scale = 0.5
+    batch_size = 128
+    epochs = 1 
