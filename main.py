d2d51ef73178559f9e4b4da8b8933cae6ffdc9ad
Paweł Pajewski <pawel.pajewski@leancdode.pl>
2024-08-27 17:01:28 +0200

    Make resampling system independent vol. 2

--- main.py
+++ main.py
@@ -6,9 +6,8 @@ from data_preprocessing.data_preparator import DataPreparator
 def main():
     audio_path = 'example_data/ryczekWav.wav'
     audio_name = os.path.basename(audio_path)
-    output_path = os.path.join(Config.dataset_root, audio_name)
     new_audio_folder = os.path.join(Config.dataset_audio_path, audio_name)
-    # AudioCutter(audio_path).cutAndAddToBaseData()
+    AudioCutter(audio_path).cutAndAddToBaseData()
     DataPreparator(new_audio_folder).prepare()
     
 
