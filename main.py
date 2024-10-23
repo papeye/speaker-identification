ecab1a763697a5824949ca0c2c3414b0f070904e
Ryczek <miryczkowski@gmail.com>
2024-10-23 14:22:56 +0200

    Corrected execution times

--- main.py
+++ main.py
@@ -13,12 +13,12 @@ from nnmodel import NNModel
 from training_type import TrainingType
 
 """ Flags for execution control"""
-TRAINING_TYPE = TrainingType.PREPARE_DATA_AND_TRAIN
+# TRAINING_TYPE = TrainingType.PREPARE_DATA_AND_TRAIN
 # TRAINING_TYPE = TrainingType.TRAIN_ONLY
-# TRAINING_TYPE = TrainingType.NO_TRAINING
+TRAINING_TYPE = TrainingType.NO_TRAINING
 
 ADD_NOISE_TO_TRAINING_DATA = False
-PREPARE_TEST_DATA = True
+PREPARE_TEST_DATA = False
 
 
 def main():
@@ -32,7 +32,7 @@ def main():
         cut_all_into_segments(train_data_dir, Config.dataset_train_audio)
 
         noises = prepareNoise() if ADD_NOISE_TO_TRAINING_DATA else None
-        finish_time_prepare = time.time()
+        time_prepare = time.time() - start_time_prepare
 
 
 
@@ -42,41 +42,46 @@ def main():
         start_time_training = time.time()
         train_ds, valid_ds = generate_train_valid_ds(noises)
         nn_model.train(train_ds, valid_ds)
-        finish_time_training = time.time()
+        time_training = time.time() - start_time_training
     else:
+        start_time_training = time.time()
         nn_model.load()
+        time_training = time.time() - start_time_training
+
 
     if PREPARE_TEST_DATA:
         start_time_test = time.time()
         for file in os.listdir(test_data_dir):
             path = os.path.join(test_data_dir, file)
             AudioCutter(path, Config.dataset_test).cut()
-        finish_time_test = time.time()
+        time_test = time.time() - start_time_test
 
-
+    start_time_predict = time.time()
     for dir in os.listdir(Config.dataset_test):
-        start_time_predict = time.time()
         path = os.path.join(Config.dataset_test, dir)
 
         test_ds = generate_test_ds(path, dir)
 
         predictions = nn_model.predict(test_ds)
-        finish_time_predict = time.time()
-
+        
         print(f"Correct speaker: {dir}")
         printPrettyDict(predictions)
-        
+    time_predict = time.time() - start_time_predict
+
+    print(f"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
     #Below - times of execution
     if TRAINING_TYPE.prepareTrainData:
-        print(f"Training data preparation took {finish_time_prepare - start_time_prepare} seconds")
+        print(f"Training data preparation took {time_prepare} seconds")
     
     if TRAINING_TYPE.train:
-        print(f"Training took {finish_time_training - start_time_training} seconds")
+        print(f"Training took {time_training} seconds")
+    else:
+        print(f"Model loading took {time_training} seconds")
         
     if PREPARE_TEST_DATA:
-        print(f"Test data preparation took {finish_time_test - start_time_test} seconds")
+        print(f"Test data preparation took {time_test} seconds")
     
-    print(f"Predictions took {finish_time_predict  - start_time_predict} seconds")
+    print(f"Predictions took {time_predict} seconds")
 
 if __name__ == "__main__":
     start_time = time.time()
