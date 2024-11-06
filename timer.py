cf134124e1679fbd5d018f7b778e3a5d743d27f3
Ryczek <miryczkowski@gmail.com>
2024-11-06 14:03:30 +0100

    fix after rebase times of execution

--- timer.py
+++ timer.py
@@ -4,32 +4,37 @@ import time
 #Class for measuring times of execution!
 class Timer:
 
+    def __init__(self):
+        self.training_time = None
+        self.prepare_train_time = None
+        self.prepare_test_time = None
+    
     def start_training(self):
-        self.start_training = time.time()
+        self.start_train = time.time()
 
     def end_training(self):
-        self.training_time = time.time()-self.start_training
+        self.training_time = time.time()-self.start_train
         
     def start_prepare_train(self):
-        self.start_prepare_train = time.time()
+        self.start_prepare = time.time()
 
     def end_prepare_train(self):
-        self.prepare_train_time = time.time()-self.start_prepare_train
+        self.prepare_train_time = time.time()-self.start_prepare
         
     def start_prepare_test(self):
-        self.start_prepare_test = time.time()
+        self.start_prepare= time.time()
 
     def end_prepare_test(self):
-        self.prepare_test_time = time.time()-self.start_prepare_test
+        self.prepare_test_time = time.time()-self.start_prepare
 
-    def start_predict(self):
+    def start_predicting(self):
         self.start_predict = time.time()
 
     def end_predict(self):
         self.predict_time = time.time()-self.start_predict
         
-    def start_execution(self):
-        self.start_predict = time.time()
+    def start_executing(self):
+        self.start_execution = time.time()
 
     def end_execution(self):
         self.execution_time = time.time()-self.start_execution
@@ -37,9 +42,9 @@ class Timer:
     def __repr__(self):
         return f"""
         Execution {self.execution_time} \n
-        Train: {self.training_time} \n
-        Prepare train: {self.prepare_train_time} \n
-        Prepare test: {self.prepare_test_time} \n
+        Train: {self.training_time if self.training_time is not None else 0} \n
+        Prepare train: {self.prepare_train_time if self.prepare_train_time is not None else 0} \n
+        Prepare test: {self.prepare_test_time if self.prepare_test_time is not None else 0} \n
         Predict: {self.predict_time} \n
         """
         
