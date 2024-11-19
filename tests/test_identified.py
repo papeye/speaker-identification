7232dd7300a8972d2d270c39be96acb77543106d
Paweł Pajewski <pawel.pajewski@leancdode.pl>
2024-11-19 13:19:22 +0100

    Add docsrings and type hints to test_identified.py

--- tests/test_identified.py
+++ tests/test_identified.py
@@ -1,8 +1,19 @@
-from speaker_identifier.training_type import TrainingType
-from speaker_identifier.speaker_identifier import SpeakerIdentifier
+from speaker_identifier import SpeakerIdentifier, TrainingType
+
+minimun_threshold = 0.5  # TODO Update to 0.9 before release
 
 
-def run_main():
+def run_predicting() -> float:
+    """
+    Trains the speaker identifier model and tests its prediction accuracy.
+
+    This function initializes the `SpeakerIdentifier` instance, trains the model
+    using example training data, and evaluates its accuracy on example test data.
+
+    Returns:
+        float: The proportion of correctly identified speakers in the test data.
+    """
+
     train_example_dir = "example_data/train_data"
     test_example_dir = "example_data/test_data"
 
@@ -22,6 +33,14 @@ def run_main():
     return correctly_identified
 
 
-def test_correctly_identifed():
-    # TODO Once we fix testing data this number needs to be increased to at least 0.9
-    assert run_main() >= 0.5
+def test_correctly_identifed() -> None:
+    """
+    Test to ensure the model meets the minimum accuracy threshold.
+
+    Asserts that the accuracy of the speaker identifier, as returned by
+    `run_predicting`, is greater than or equal to the minimum threshold.
+
+    Raises:
+        AssertionError: If the accuracy is below the minimum threshold.
+    """
+    assert run_predicting() >= minimun_threshold
