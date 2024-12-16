2731247bbd6845db8d1b6c40f3ebe1404d21bf76
StDevon <michal.m.lukawski@gmail.com>
2024-12-16 12:57:33 +0100

    reduce the NN, choose #of steps per epoch so that accuracy>0.85

--- speaker_identifier/nnmodel.py
+++ speaker_identifier/nnmodel.py
@@ -70,8 +70,8 @@ class NNModel:
         x = self.__residual_block(inputs, 16, 2)
         x = self.__residual_block(x, 32, 2)
         x = self.__residual_block(x, 64, 3)
-        x = self.__residual_block(x, 128, 3)
-        x = self.__residual_block(x, 128, 3)
+        # x = self.__residual_block(x, 128, 3)
+        # x = self.__residual_block(x, 128, 3)
 
         x = keras.layers.AveragePooling1D(pool_size=3, strides=3)(x)
         x = keras.layers.Flatten()(x)
