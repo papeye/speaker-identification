08cc885474a0c1690bbf9f427ffd695bc2dfbccd
Paweł Pajewski <pawelpajewski@Pawes-Mac-mini.lan>
2025-01-14 11:24:45 +0100

    Remove display predicitions method

--- speaker_identifier/__init__.py
+++ speaker_identifier/__init__.py
@@ -1,4 +1,3 @@
 from .speaker_identifier import SpeakerIdentifier
-from .helpers import display_predictions
 
-__all__ = ["SpeakerIdentifier", "display_predictions"]
+__all__ = ["SpeakerIdentifier"]
