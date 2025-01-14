fff09ffcec5339df0cb8131e8432af1ae8dca5d7
Paweł Pajewski <pawel.pajewski@leancdode.pl>
2025-01-14 17:14:40 +0100

    t

--- example.py
+++ example.py
@@ -2,15 +2,18 @@ from speaker_identifier import *
 
 
 """ Flags for execution control"""
-TRAINING_VAD = True # whether to use VAD for training data
-PREDICTING_VAD = True # whether to use VAD for predicting data
+TRAINING_VAD = True  # whether to use VAD for training data
+PREDICTING_VAD = True  # whether to use VAD for predicting data
 
 
 def main():
     train_example_dir = "example_data/train_data"
     test_example_dir = "example_data/test_data"
 
-    user1 = SpeakerIdentifier(model_name="user1", training_ds_dir=train_example_dir)
+    user1 = SpeakerIdentifier(
+        model_name="user1",
+        training_ds_dir=train_example_dir,
+    )
 
     user1.train(
         train_data_dir=train_example_dir,
